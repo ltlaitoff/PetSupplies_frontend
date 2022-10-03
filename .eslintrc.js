73bc324d9893558a7b6f23a9330248cde13219c6
Ivan Baluta <ltlaitoff@gmail.com>
2022-10-03 19:17:28 +0300

    chore(eslint): Added ignore debugger and console in dev mode

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,8 @@ module.exports = {
 	},
 	plugins: ['vue', '@typescript-eslint', 'prettier'],
 	rules: {
-		'no-console': 'warn',
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'prettier/prettier': 'error',
 		'arrow-body-style': 'off',
 		'prefer-arrow-callback': 'off'

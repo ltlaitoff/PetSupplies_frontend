537f670b4f27732c5e3812ffc7763e7ddbeb1445
Ivan Baluta <ltlaitoff@gmail.com>
2022-10-09 00:29:46 +0300

    refactor: Commented PerformancePage styles

--- src/pages/PerformancePage/PerformancePage.vue
+++ src/pages/PerformancePage/PerformancePage.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import MainLogo from '../../components/LogoMain/LogoMain.vue'
+import SearchBox from '../../components/SearchBox/SearchBox.vue'
 </script>
 
 <template>
 	<div :class="$style.perf">
 		<MainLogo />
+		<SearchBox />
 	</div>
 </template>
 

12fb86866b2591e08a6cfc408eb089b56e91a4de
Ivan Baluta <ltlaitoff@gmail.com>
2022-10-09 00:46:13 +0300

    feat: Added submit emit in SearchBox component

--- src/components/SearchBox/SearchBox.vue
+++ src/components/SearchBox/SearchBox.vue
@@ -2,16 +2,38 @@
 /// <reference types="vite-svg-loader" />
 import Search from '../../assets/icons/search.svg?component'
 
-import { ref, watch } from 'vue'
+import { ref, watch, defineEmits } from 'vue'
+
+const emit = defineEmits({
+	submit: (value: string) => {
+		if (value === '') {
+			console.warn('Invalid submit event payload!')
+			return false
+		}
+
+		return true
+	}
+})
 
 const inputText = ref('')
 const inputError = ref(false)
+const inputTemplate = ref<HTMLInputElement | null>(null)
+
+function onSubmitButton() {
+	if (inputTemplate.value) {
+		inputTemplate.value.focus()
+	}
+
+	onSubmit()
+}
 
 function onSubmit() {
 	if (inputText.value === '') {
 		inputError.value = true
 		return
 	}
+
+	emit('submit', inputText.value)
 }
 
 watch(inputText, async (newText: string) => {
@@ -33,11 +55,13 @@ watch(inputText, async (newText: string) => {
 			<button
 				:class="$style.iconButton"
 				tabindex="-1"
-				@click="onSubmit"
+				type="button"
+				@click="onSubmitButton"
 			>
 				<Search :class="$style.icon" />
 			</button>
 			<input
+				ref="inputTemplate"
 				v-model="inputText"
 				:class="[$style.input, `${inputError ? $style.inputError : ''}`]"
 				placeholder="Write something"
--- src/pages/PerformancePage/PerformancePage.vue
+++ src/pages/PerformancePage/PerformancePage.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import MainLogo from '../../components/LogoMain/LogoMain.vue'
 import SearchBox from '../../components/SearchBox/SearchBox.vue'
+
+function onSearchBoxSubmit(value: string) {
+	console.log(`SearchBox @submit: ${value}`)
+}
 </script>
 
 <template>
 	<div :class="$style.perf">
 		<MainLogo />
-		<SearchBox />
+		<SearchBox @submit="onSearchBoxSubmit" />
 	</div>
 </template>
 

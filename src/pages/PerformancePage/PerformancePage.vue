46a83473b13442822150067df413802fbd5e6310
Ivan Baluta <ltlaitoff@gmail.com>
2022-10-09 23:55:06 +0300

    feat: Added LanguageSwith on perf page

--- src/pages/PerformancePage/PerformancePage.vue
+++ src/pages/PerformancePage/PerformancePage.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
 import MainLogo from '../../components/LogoMain/LogoMain.vue'
 import SearchBox from '../../components/SearchBox/SearchBox.vue'
+import LanguageSwitch from '../../components/LanguageSwitch/LanguageSwitch.vue'
 
 function onSearchBoxSubmit(value: string) {
 	console.log(`SearchBox @submit: ${value}`)
+}
+
+function onLanguageChange(value: { id: number; languageCode: string }) {
+	console.log(`Language switch: ${JSON.stringify(value)}`)
 }
 </script>
 
 <template>
 	<div :class="$style.perf">
 		<MainLogo />
+		<div :class="$style.divider" />
 		<SearchBox @submit="onSearchBoxSubmit" />
+		<div :class="$style.divider" />
+		<LanguageSwitch @change="onLanguageChange" />
 	</div>
 </template>
 

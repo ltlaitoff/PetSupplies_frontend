5a5ff8a03c171173383aaed352d02ee7dfe17d11
Ivan Baluta <ltlaitoff@gmail.com>
2022-10-10 22:47:23 +0300

    feat: Created FooterMain component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HeaderMain from './components/HeaderMain/HeaderMain.vue'
+import FooterMain from './components/FooterMain/FooterMain.vue'
 </script>
 
 <template>
 	<HeaderMain />
 	<router-view />
+	<FooterMain />
 </template>
 
 <style></style>

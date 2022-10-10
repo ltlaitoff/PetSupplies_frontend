9a1d08be52817266d9e7463f2441c4f157105811
Ivan Baluta <ltlaitoff@gmail.com>
2022-10-10 23:08:58 +0300

    feat: Created basic vuex store

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import './style/style.scss'
 import App from './App.vue'
 import router from './router'
+import { store, key } from './store'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store, key)
 
 app.mount('#app')

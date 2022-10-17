20189d0f310e200d450e6aa0f0eccf7c0882c69f
Ivan Baluta <ltlaitoff@gmail.com>
2022-10-17 23:09:11 +0300

    feat: Connect storage with vuex store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import HeaderMain from './components/HeaderMain/HeaderMain.vue'
 import FooterMain from './components/FooterMain/FooterMain.vue'
+import { useStore } from './store/store'
+
+const store = useStore()
+store.commit('initialiseStore')
 </script>
 
 <template>
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,8 @@ import { InjectionKey } from 'vue'
 import { createStore, useStore as baseUseStore, Store } from 'vuex'
 import { RootState } from './store.interfaces'
 import { themeModule } from './theme'
+import { getValue } from '../utils/storage'
+import { setValue } from '../utils/storage/storage'
 
 // https://vuex.vuejs.org/guide/typescript-support.html#typing-usestore-composition-function
 export const key: InjectionKey<Store<RootState>> = Symbol()
@@ -15,17 +17,18 @@ export const store = createStore<RootState>({
 	// },
 	modules: {
 		theme: themeModule
+	},
+	mutations: {
+		initialiseStore(state) {
+			const data = getValue()
+
+			this.replaceState(Object.assign(state, data))
+		}
 	}
-	// mutations: {
-	// 	changeTheme(state, data: Theme) {
-	// 		state.theme = data
-	// 	}
-	// },
-	// actions: {
-	// 	changeTheme(state, data: Theme) {
-	// 		this.commit('changeTheme', data)
-	// 	}
-	// }
+})
+
+store.subscribe((_, state) => {
+	setValue(state)
 })
 
 export function useStore() {
